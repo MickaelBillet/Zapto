ac1d39c47f76b4eba21a982419d5cd39f9b1c588
MickaelBillet <mickael.billet@gmail.com>
2023-11-02 17:58:11 +0100

    docker

--- Connect.WebServer/Scripts/docker.ps1
+++ Connect.WebServer/Scripts/docker.ps1
@@ -1,4 +1,4 @@
-﻿cd ../../..
+﻿cd ../..
 
 Write-Output "Docker login..."
 
@@ -10,7 +10,7 @@ if($LASTEXITCODE -eq 0)
     # Success
     Write-Output "Docker Login Success"
     Write-Output "Docker Build Progress..."
-    $result = docker build -t mickabdocker/connectwebserver:v3 -f ./Zapto/Connect.WebServer/Dockerfile .
+    $result = docker build -t mickabdocker/connectwebserver:v3 -f ./Connect.WebServer/Dockerfile .
 
     # Evaluate success/failure
     if($LASTEXITCODE -eq 0)
