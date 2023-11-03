a1ee2e3379e55cdf82f9c58aae13c10d006c8ff7
MickaelBillet <mickael.billet@gmail.com>
2023-11-03 11:23:03 +0100

    test

---
+++ Connect.Test.Data/Supervisor/SupervisorLogTest.cs
@@ -0,0 +1,73 @@
+﻿using Connect.Data.Supervisors;
+using Connect.Model;
+using Framework.Core.Base;
+
+namespace Connect.Test.Data.Supervisor
+{
+    public class SupervisorLogTest : SupervisorTest
+    {
+        [Fact]
+        public async Task CollectionGetLogsCollection()
+        {
+            ISupervisor supervisor = new Connect.Data.Supervisors.Supervisor(this.ServiceProvider, this.CreateContext());
+            IEnumerable<Logs> logs = await supervisor.GetLogsCollection();
+
+            Assert.NotNull(logs);
+            Assert.Collection(logs,
+                                item1 => Assert.Equal("0", item1.Id));
+        }
+
+        [Fact]
+        public async Task GetLogsInf24H()
+        {
+            ISupervisor supervisor = new Connect.Data.Supervisors.Supervisor(this.ServiceProvider, this.CreateContext());
+            IEnumerable<Logs> logs = await supervisor.GetLogsInf24H();
+
+            Assert.NotNull(logs);
+        }
+
+        [Fact]
+        public async Task AddLog()
+        {
+            this.ResetDatabase();
+            ISupervisor supervisor = new Connect.Data.Supervisors.Supervisor(this.ServiceProvider, this.CreateContext());
+            ResultCode code = await supervisor.AddLog(new Logs { Id = "1"});
+            IEnumerable<Logs> logs = await supervisor.GetLogsCollection();
+            
+            Assert.NotNull(logs);
+            Assert.True(code == ResultCode.Ok);
+            Assert.True(logs.Count() == 2);
+        }
+
+        [Fact]
+        public async Task LogExists()
+        {
+            ISupervisor supervisor = new Connect.Data.Supervisors.Supervisor(this.ServiceProvider, this.CreateContext());
+            ResultCode code = await supervisor.LogExists("0");
+
+            Assert.True(code == ResultCode.Ok);
+        }
+
+        [Fact]
+        public async Task GetLogs()
+        {
+            ISupervisor supervisor = new Connect.Data.Supervisors.Supervisor(this.ServiceProvider, this.CreateContext());
+            Logs logs = await supervisor.GetLogs("0");
+
+            Assert.NotNull(logs);
+        }
+
+        [Fact]
+        public async Task DeleteLogs()
+        {
+            this.ResetDatabase();
+            ISupervisor supervisor = new Connect.Data.Supervisors.Supervisor(this.ServiceProvider, this.CreateContext());
+            ResultCode code = await supervisor.DeleteLogs(new Logs { Id = "0" });
+            IEnumerable<Logs> logs = await supervisor.GetLogsCollection();
+
+            Assert.NotNull(logs);
+            Assert.True(code == ResultCode.Ok);
+            Assert.True(logs.Count() == 0);
+        }
+    }
+}
---
+++ Connect.Test.Data/Supervisor/SupervisorTest.cs
@@ -0,0 +1,46 @@
+﻿using Connect.Data.DataContext;
+using Connect.Data.Entities;
+using Connect.Data.Repository;
+using Framework.Core.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Connect.Test.Data.Supervisor
+{
+    public abstract class SupervisorTest
+    {
+        protected IServiceProvider ServiceProvider { get; }
+        protected DbContextOptions ContextOptions { get; }
+
+        public SupervisorTest() 
+        { 
+            ServiceCollection services = new ServiceCollection();
+            services.AddRepositories();
+            this.ServiceProvider = services.BuildServiceProvider();
+
+            this.ContextOptions = new DbContextOptionsBuilder()
+                                                .UseInMemoryDatabase("ConnectDatabase")
+                                                .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                                                .Options;
+
+            this.ResetDatabase();
+        }
+
+        protected void ResetDatabase()
+        {
+            using (ConnectContextInMemory context = new ConnectContextInMemory(this.ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.AddRange(
+                    new LogsEntity { Id = "0", CreationDateTime = Clock.Now, Level = "0", Properties = "Toto" }); ;
+
+                context.SaveChanges();
+            }
+        }
+
+        public ConnectContextInMemory CreateContext() => new ConnectContextInMemory(this.ContextOptions);
+    }
+}
