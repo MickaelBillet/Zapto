356357cedf7dc328d867f82825d233b71fb1e2f4
Billet Mickael <mickael.billet@gmail.com>
2024-02-11 08:07:14 +0100

    feat - v2

--- Connect.WebServer/Scripts/docker.ps1
+++ Connect.WebServer/Scripts/docker.ps1
@@ -10,7 +10,7 @@ if($LASTEXITCODE -eq 0)
     # Success
     Write-Output "Docker Login Success"
     Write-Output "Docker Build Progress..."
-    $result = docker build -t mickabdocker/connectwebserver:v1 -f ./Connect.WebServer/Dockerfile .
+    $result = docker build -t mickabdocker/connectwebserver:v2 -f ./Connect.WebServer/Dockerfile .
 
     # Evaluate success/failure
     if($LASTEXITCODE -eq 0)
@@ -18,7 +18,7 @@ if($LASTEXITCODE -eq 0)
         # Success
         Write-Output "Docker Build Success"
         Write-Output "Docker Push Progress..."
-        $result = docker push mickabdocker/connectwebserver:v1
+        $result = docker push mickabdocker/connectwebserver:v2
      
         # Evaluate success/failure
         if($LASTEXITCODE -eq 0)
