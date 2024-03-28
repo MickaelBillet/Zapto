e7c29735a0e2042ad8df443bb4e7e96f5741d80d
MickaelBillet <mickael.billet@gmail.com>
2024-03-28 17:15:34 +0100

    Tests

--- WeatherZapto.Data.Supervisors.Tests/SupervisorCallTest.cs
+++ WeatherZapto.Data.Supervisors.Tests/SupervisorCallTest.cs
@@ -25,27 +25,27 @@ namespace WeatherZapto.Data.Supervisors.Tests
             Assert.True(code == ResultCode.Ok);
         }
 
-        //[Fact]
-        //public async Task GetDayCallsCount()
-        //{
-        //    //Arrange
-        //    await this.Initialyse();
+        [Fact]
+        public async Task GetDayCallsCount()
+        {
+            //Arrange
+            await this.Initialyse();
 
-        //    //Act
-        //    long? call = null;
-        //    IDalSession session = this.HostApplication.Services.GetRequiredService<IDalSession>();
-        //    IRepositoryFactory repositoryFactory = this.HostApplication.Services.GetRequiredService<IRepositoryFactory>();
-        //    ISupervisorCall supervisor = new SupervisorCall(session, repositoryFactory);
-        //    ResultCode code = await supervisor.AddCallOpenWeather();
-        //    if (code == ResultCode.Ok)
-        //    {
-        //        call = await supervisor.GetDayCallsCount(Clock.Now);
-        //    }
+            //Act
+            long? call = null;
+            IDalSession session = this.HostApplication.Services.GetRequiredService<IDalSession>();
+            IRepositoryFactory repositoryFactory = this.HostApplication.Services.GetRequiredService<IRepositoryFactory>();
+            ISupervisorCall supervisor = new SupervisorCall(session, repositoryFactory);
+            ResultCode code = await supervisor.AddCallOpenWeather();
+            if (code == ResultCode.Ok)
+            {
+                call = await supervisor.GetDayCallsCount(Clock.Now);
+            }
 
-        //    //Assert
-        //    Assert.True(code == ResultCode.Ok);
-        //    Assert.True(call == 1);
-        //}
+            //Assert
+            Assert.True(code == ResultCode.Ok);
+            Assert.True(call == 1);
+        }
 
         //[Fact]
         //public async Task GetLast30DaysCallsCount()
