fd8434b359e74389a0f01eda444ad7aa81c7be74
Billet Mickael <mbillet@novacor.fr>
2024-10-02 12:06:38 +0200

    docker

--- WeatherZapto.WebServer/Scripts/weatherzapto.docker.ps1
+++ WeatherZapto.WebServer/Scripts/weatherzapto.docker.ps1
@@ -10,7 +10,7 @@ if($LASTEXITCODE -eq 0)
     # Success
     Write-Output "Docker Login Success"
     Write-Output "Docker Build Progress..."
-    $result = docker build -t mickabdocker/weatherzaptowebserver:v2 -f ./WeatherZapto.WebServer/Dockerfile .
+    $result = docker build --tag mickabdocker/weatherzaptowebserver:v2 --file ./WeatherZapto.WebServer/Dockerfile .
 
     # Evaluate success/failure
     if($LASTEXITCODE -eq 0)
